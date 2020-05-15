7cb625b9f61a26847de0e200eaf0e69c37933cea
Tristan Stutzman <tstutzman@beardlysoft.com>
2020-05-15 13:42:38 -0700

    custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
+  root 'pages#home'
 end
