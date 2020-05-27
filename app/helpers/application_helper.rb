ea535539c6c393840e8d1cc020897de5a3e75329
Tristan Stutzman <tstutzman@beardlysoft.com>
2020-05-27 14:36:43 -0700

    add ruby gem for copyright info in footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,8 @@ module ApplicationHelper
     content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    BeardlyViewTool::Renderer.copyright 'Tristan Stutzman', 'All rights reserved'
+  end
 end
