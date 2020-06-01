275a7ac46c8d0e91236a73f8c9466e5758f74864
Tristan Stutzman <tstutzman@beardlysoft.com>
2020-06-01 14:59:26 -0700

    started portfolio styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'bootstrap'
+// import './src/application.scss'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-
+require("bootstrap/dist/js/bootstrap")
 
 //= require jquery3
 //= require popper
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,17 @@
   )
 end
 
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
-      title: "My blog post #{blog}",
-      body: "is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      topic_id: Topic.last.id
+    title: "My Blog Post #{blog}",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
   )
 end
+
+puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
@@ -19,28 +23,34 @@ end
   )
 end
 
+puts "5 skills created"
+
 8.times do |portfolio_item|
   Portfolio.create!(
-    title: "Portfolio title #{portfolio_item}",
+    title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
-    body: "text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and",
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/300x200"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
 1.times do |portfolio_item|
   Portfolio.create!(
-    title: "Portfolio title #{portfolio_item}",
+    title: "Portfolio title: #{portfolio_item}",
     subtitle: "Angular",
-    body: "text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and",
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/300x200"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
-3.times.do |technology|
+puts "9 portfolio items created"
+
+3.times do |technology|
   Portfolio.last.technologies.create!(
-    name: "Technology #{technology}",
+    name: "Technology #{technology}"
   )
 end
+
+puts "3 technologies created"
