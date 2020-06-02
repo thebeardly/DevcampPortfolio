ef88211e9cfbb9383c4e809f5002e23a4b3af8b0
Tristan Stutzman <tstutzman@beardlysoft.com>
2020-06-02 09:07:46 -0700

    installed jquery ui and sortable library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,15 @@ require("channels")
 require('jquery')
 require("bootstrap/dist/js/bootstrap")
 
+require("custom/home")
+
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery-ui
+//= require custom/home
+//= require tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
