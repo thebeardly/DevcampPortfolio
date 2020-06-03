880370a522d827d45764ca20e36d64bea9398c11
Tristan Stutzman <tstutzman@beardlysoft.com>
2020-06-03 08:26:05 -0700

    add image uploading

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,6 +7,10 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   acts_as_list
+  
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+  
 
   def self.angular
     where(subtitle: 'Angular')
