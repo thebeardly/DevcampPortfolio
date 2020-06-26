9e616d27c26a4c509f7143a10ed309bb6dec8b89
Tristan Stutzman <tristan.stutzman@gmail.com>
2020-06-26 10:37:42 -0700

    added mailer config and mobile nav issue fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "DevcampPortfolio_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'tstutzman.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
